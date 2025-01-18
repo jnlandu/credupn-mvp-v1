8f04859568bf23b76477821d66f1ecf2103fae04
= <=>
2025-01-18 18:38:22 +0100

    update global.css

--- app/page.tsx
+++ app/page.tsx
@@ -26,9 +26,11 @@ export default function Home() {
         <div className="absolute inset-0 bg-black/50 " />
         <div className="relative z-10 flex flex-col items-center justify-center h-full text-center px-4">
           <h1 className="text-4xl md:text-6xl font-bold text-white mb-2">
-            Centre de Recherche  Interdisciplinaire <br/>
-            CRIDUPN
+            Université Pédagogique Nationale
           </h1>
+          <h2 className="text-4xl md:text-6xl font-bold text-white mb-2">
+            Centre de Recherche Interdisciplinaire
+          </h2>
           <p className="text-xl text-white mb-8 max-w-2xl mt-0">
             Excellence en recherche pour le développement de l'éducation en République Démocratique du Congo
           </p>
@@ -213,7 +215,7 @@ export default function Home() {
     <h2 className="text-3xl font-bold text-center text-black mb-12">Nos Membres</h2>
     <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
       {members.map((reviewer, index) => (
-        <Card key={index} className="overflow-hidden">
+        <Card key={index} className="overflow-hidden bg-black">
           <div className="relative h-64 mb-4">
             <Image
               src={reviewer.image}
