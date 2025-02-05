b25d6eab9371fe29f4be0fb5be629ba32a57d67b
= <=>
2025-02-05 09:08:25 +0100

    add backend to execute concurrently

--- utils/api.ts
+++ utils/api.ts
@@ -1,2 +1,2 @@
 // utils/api.ts
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000'
+export const API_URL = process.env.NEXT_PUBLIC_FASTAPI_URL || 'http://localhost:8000'
